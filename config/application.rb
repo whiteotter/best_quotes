ae2289665fb3af73b733de28ad553e13e3012549
Drew Kitch <drew.kitch@gmail.com>
2013-07-29 14:14:57 -0700

    routing fun, http redirects

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,15 @@
 require 'rulers'
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+require 'quotes_controller'
 
 module BestQuotes
   class Application < Rulers::Application
+    def call(env)
+      if env['PATH_INFO'] == '/'
+        # env['PATH_INFO'] = '/quotes/a_quote'
+        return [307, {'Content-Type' => 'text/html', 'Location' => 'quotes/a_quote'}, []]
+      end
+      super
+    end
   end
 end
